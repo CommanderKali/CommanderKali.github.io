c5d9cfe998d3d7cbf27f91c49a5e3e81c7fa9309
Dan Thompson <awkwardpineapple123@gmail.com>
2021-09-30 18:52:42 -0400

    Update main.js

--- main.js
+++ main.js
@@ -14482,6 +14482,20 @@ window.onload=function()
 				'Hey, Orteil here. Cheated cookies taste awful... or do they?',
 			])+' ===]');
 			Game.Load();
+			// ==UserScript==
+			// @name Cookie Monster
+			// @include /https?://orteil.dashnet.org/cookieclicker/
+			// ==/UserScript==
+
+			const readyCheck = setInterval(() => {
+  			  const Game = unsafeWindow.Game;
+
+ 		   	  if (typeof Game !== 'undefined' && typeof Game.ready !== 'undefined' && Game.ready) {
+   			    Game.LoadMod('https://cookiemonsterteam.github.io/CookieMonster/dist/CookieMonster.js');
+   			    clearInterval(readyCheck);
+ 			   }
+			}, 1000);
+
 			//try {Game.Load();}
 			//catch(err) {console.log('ERROR : '+err.message);}
 		}
